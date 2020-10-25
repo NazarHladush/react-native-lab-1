9e36dc6c95e97644eda78854f0cee6bf0fa8c5df
Nazar Hladysh <nazar.gladish@gmail.com>
2020-10-25 21:04:13 +0200

    little fix

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Button } from 'react-native'
+import {firebase} from '../../firebase/config'
 
 export default function HomeScreen(props) {
+
+    const onPressLearnMore = () => {
+        firebase.auth().signOut().then(() => {
+            props.navigation.navigate('Home')
+        })
+    }
+
     return (
-        <View>
-            <Text>Home Screen {props.extraData}</Text>
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+            <Text>Welcome, {props.extraData.username}</Text>
         </View>
     )
 }
